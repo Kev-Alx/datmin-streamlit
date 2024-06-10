c132ec389d419bd37e67f6b4ab45e49eced9b774
Kev-Alx <120734048+Kev-Alx@users.noreply.github.com>
2024-06-10 15:22:33 +0700

    init

--- Pages/Form.py
+++ Pages/Form.py
@@ -3,24 +3,19 @@ import pandas as pd
 
 def main():
   st.title("New Bill Form")
-  menu = ["Home", "About"]
-  choice = st.sidebar.selectbox("Menu", menu)
+  
+  with st.form(key = 'myform', clear_on_submit=True):
+    BillNo = st.text_input("Bill Number: ")
+    ItemName = st.text_input("Item Name: ")
+    Quantity = st.text_input("Quantity: ")
+    Date = st.date_input("Date: ")
+    Price = st.text_input("Price: ")
+    CustID = st.text_input("Customer Id: ")
+    submit_button = st.form_submit_button(label='Submit')
     
-  if choice == "Home":
-    st.subheader("Fill the form")
-    
-    with st.form(key = 'myform', clear_on_submit=True):
-      BillNo = st.text_input("Bill Number: ")
-      ItemName = st.text_input("Item Name: ")
-      Quantity = st.text_input("Quantity: ")
-      Date = st.date_input("Date: ")
-      Price = st.text_input("Price: ")
-      CustID = st.text_input("Customer Id: ")
-      submit_button = st.form_submit_button(label='Submit')
-    
-    if submit_button:
-      save_to_txt(BillNo, ItemName, Quantity, Date, Price, CustID)
-      st.success()
+  if submit_button:
+    save_to_txt(BillNo, ItemName, Quantity, Date, Price, CustID)
+    st.success()
       
 
 
